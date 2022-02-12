5da3474427305e8443c4ca8f47339e0660c07dc0
VietNguyenWolf <nguyenhoangviethustk63@gmail.com>
2022-02-12 19:26:01 +0700

    snake_game ver 1

---
+++ main.py
@@ -0,0 +1,58 @@
+import imp
+from turtle import Screen
+import time 
+from snake import Snake
+from food import Food
+from scoreboard import Scoreboard
+
+screen  = Screen()
+screen.setup(width=600,height=600)
+screen.bgcolor('black')
+screen.title("Snake Game")
+screen.tracer(0)
+
+snake = Snake()
+food = Food()
+scoreboard = Scoreboard()
+
+screen.listen()
+screen.onkey(key='Up',fun=snake.move_up)
+screen.onkey(key='Down',fun=snake.move_down)
+screen.onkey(key='Right',fun=snake.move_right)
+screen.onkey(key='Left',fun=snake.move_left)
+
+
+
+
+
+
+
+screen.update()
+
+game_is_on = True
+while game_is_on:
+    screen.update()
+    time.sleep(0.1)
+
+    snake.snake_move()
+
+    #detect collision with food
+    if snake.head.distance(food) < 15:
+        food.refresh_food()
+        scoreboard.increase_score()
+        snake.snake_grow_up()
+    
+    # detect collision with wall
+    if snake.head.xcor() > 280 or snake.head.xcor() < -300 \
+        or snake.head.ycor() >300 or snake.head.ycor() <-280:
+        game_is_on = False
+        scoreboard.game_over()
+    
+    #detect collision with tail
+    for segment in snake.full_body[1:]:
+        if snake.head.distance(segment) < 10:
+            game_is_on = False
+            scoreboard.game_over()
+
+
+screen.exitonclick()
---
+++ snake.py
@@ -0,0 +1,58 @@
+import imp
+from turtle import Turtle
+
+STARTING_POS = [(0,0),(-20,0),(-40,0)]
+snake_body= []
+MOVE_DISTANCE = 20
+UP = 90
+DOWN = 270
+RIGHT = 0
+LEFT = 180
+class Snake():
+    def __init__(self):
+        self.full_body = []
+        self.create_snake()
+        self.head  = self.full_body[0]
+    
+    def create_snake(self):
+        for pos in STARTING_POS:
+            new_segment = Turtle(shape='square')
+            new_segment.color('white')
+            new_segment.penup()
+            new_segment.goto(pos)
+            self.full_body.append(new_segment)
+
+    
+    def snake_move(self):
+        for seg_num in range(len(self.full_body)-1,0,-1):
+            new_x = self.full_body[seg_num-1].xcor()
+            new_y = self.full_body[seg_num-1].ycor()
+            self.full_body[seg_num].goto(new_x,new_y)
+        self.head.forward(MOVE_DISTANCE)
+
+    def move_up(self):
+        if self.head.heading() != DOWN:
+            self.head.setheading(UP)
+    def move_down(self):
+        if self.head.heading() != UP:
+            self.head.setheading(DOWN)
+    def move_right(self):
+        if self.head.heading() != LEFT:
+            self.head.setheading(RIGHT)
+    def move_left(self):
+        if self.head.heading() != RIGHT:
+            self.head.setheading(LEFT)
+
+    def snake_grow_up(self):
+        # add new seg ment to snake
+        self.add_segment(self.full_body[-1].position())
+
+    def add_segment(self,position):
+        new_segment = Turtle(shape='square')
+        new_segment.color('white')
+        new_segment.penup()
+        new_segment.goto(position)
+        self.full_body.append(new_segment)
+        
+
+
